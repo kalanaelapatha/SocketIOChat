66a485ece0f5236e06147c45f636beab98f460ef
kalanaelapatha <38855998+kalanaelapatha@users.noreply.github.com>
2019-12-11 14:01:09 +0530

    server connection and disconnection with IO

--- server.js
+++ server.js
@@ -5,9 +5,27 @@ var io = require("socket.io").listen(server);
 
 users = [];
 connections = [];
-
+//basic server
 server.listen(process.env.PORT || 3000);
 console.log('Server Running....');
 app.get('/', function(req,res){
         res.sendFile(__dirname + '/index.html');
+}); 
+//Open connection with Socket IO
+
+  
+io.sockets.on('connections', function(socket){
+
+        connections.push(socket);
+        console.log('Server Connected with : %s Socckets', connections.length); 
+
+        //Disconnection about socket
+        
+        connections.splice(connections.indexOf(socket), 1);
+        console.log(' Disconnected: %s sockets connected', connections.length); //if someone disconnect we can see available users
+        
+
+
 });
+
+
